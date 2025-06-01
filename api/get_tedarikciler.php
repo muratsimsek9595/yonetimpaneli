cf086913ee2aca123c700ea907a7e429c4f8985d
muratsimsek95@gmail.com <muratsimsek95@gmail.com>
2025-06-01 14:31:01 +0300

    .

--- api/get_tedarikciler.php
+++ api/get_tedarikciler.php
@@ -1,10 +1,9 @@
 <?php
+ob_start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *"); // Geliştirme için, canlıda daha kısıtlı olabilir
 
 require_once '../config/db_config.php'; // Veritabanı bağlantısını include et
 
@@ -17,12 +16,21 @@ if ($result) {
         while($row = $result->fetch_assoc()) {
             $tedarikciler[] = $row;
         }
+        ob_clean();
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
         echo json_encode($tedarikciler);
     } else {
+        ob_clean();
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
         echo json_encode([]); // Boş dizi döndür, tedarikçi yoksa
     }
 } else {
     // Sorgu hatası durumunda
+    ob_clean();
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
     http_response_code(500); // Internal Server Error
     echo json_encode(
         array("message" => "Tedarikçiler getirilirken bir hata oluştu.", "error" => $conn->error)
