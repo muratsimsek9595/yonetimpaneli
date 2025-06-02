f70dabba3bce121aee90c41f3c5b3a877f4dd2f0
muratsimsek95@gmail.com <muratsimsek95@gmail.com>
2025-06-02 21:15:20 +0300

    duzeldikten sonraki guncelleme

--- api/teklifler.php
+++ api/teklifler.php
@@ -1,11 +1,11 @@
 <?php
-header("Content-Type: application/json; charset=UTF-8");
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../config/db_config.php';
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once '/home/hsnplant/public_html/demo/config/db_config.php';
 
 if ($conn === null || $conn->connect_error) {
     http_response_code(503);
